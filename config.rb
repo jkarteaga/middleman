942c7878e65cfd398eb95956c4aacad0d99cb395
Jon Koldo Arteaga <jkarteaga@gmail.com>
2017-08-14 17:48:19 +0200

    Update config.rb

--- config.rb
+++ config.rb
@@ -1,35 +1,69 @@
+require 'builder'
+
+###
+# Blog settings
+###
+
+# Time.zone = "UTC"
+
+###
+# Markdown
+###
+
+set :markdown_engine, :redcarpet
+set :markdown, :fenced_code_blocks => true, :smartypants => true
+
+###
+# Code highlighting
+###
+
+activate :rouge_syntax
+
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  blog.prefix = "posts"
+
+  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+  # blog.layout = "layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 50
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  blog.default_extension = ".md"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  blog.paginate = true
+  blog.per_page = 50
+  blog.page_link = "page/{num}"
+end
+
+# pretty_urls
 activate :directory_indexes
+
+activate :livereload
+
+page "/feed.xml", layout: false
+page "/sitemap.xml", :layout => false
+
+# Sitemap
+set :url_root, 'https://contractor-rhinoceros-10480.netlify.com'
+activate :search_engine_sitemap
 
 ###
 # Compass
 ###
 
-# Susy grids in Compass
-# First: gem install compass-susy-plugin
-# require 'susy'
-
 # Change Compass configuration
 # compass_config do |config|
 #   config.output_style = :compact
 # end
-
-###
-# Deploy
-###
-
-# activate :deploy do |deploy|
-#   deploy.method = :git
-#   # Optional Settings
-#   # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
-#   # deploy.branch   = 'custom-branch' # default: gh-pages
-#   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
-#   # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
-# end
-
-###
-# HAML
-###
-set :haml, ugly: true
 
 ###
 # Page options, layouts, aliases and proxies
@@ -38,20 +72,19 @@ set :haml, ugly: true
 # Per-page layout changes:
 #
 # With no layout
-# page "/path/to/file.html", :layout => false
+# page "/path/to/file.html", layout: false
 #
 # With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
+# page "/path/to/file.html", layout: :otherlayout
 #
 # A path which all have the same layout
 # with_layout :admin do
-#   page "/admin/"
+#   page "/admin/*"
 # end
 
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
 
 ###
 # Helpers
@@ -59,6 +92,9 @@ set :haml, ugly: true
 
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+# activate :livereload
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -73,35 +109,33 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
-# Add bower's directory to sprockets asset path
-after_configuration do
-  @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
-  sprockets.append_path File.join "#{root}", @bower_config['directory']
-end
-
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  activate :minify_css
+  # activate :minify_css
 
   # Minify Javascript on build
-  activate :minify_javascript
+  # activate :minify_javascript
 
   # Enable cache buster
-  # activate :cache_buster
-
-  activate :asset_hash
+  # activate :asset_hash
 
   # Use relative URLs
-  activate :relative_assets
-
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
+  # activate :relative_assets
 
   # Or use a different image path
-  # set :http_path, "/Content/images/"
+  # set :http_prefix, "/Content/images/"
+  #
 
-  activate :favicon_maker
+  # meta tag helper
+  activate :meta_tags
+end
+
+# move your redirects
+after_build do |builder|
+  src = File.join(config[:source],"_redirects")
+  dst = File.join(config[:build_dir],"_redirects")
+  builder.source_paths << File.dirname(__FILE__)
+  builder.copy_file(src,dst)
+  puts "Done building"
 end
